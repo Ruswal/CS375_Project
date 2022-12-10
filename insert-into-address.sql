fd851c1c4947867bae9535aa22990f71c2586703
FLUXxD <wadiaraj7@gmail.com>
2022-12-09 23:37:17 -0600

    address of all user added address of each user

--- insert-into-address.sql
+++ insert-into-address.sql
@@ -1,1 +1,12 @@
-insert into Addresses () Values ();
+insert into Addresses (u_id, line1, line2, city, postcode, province, country) Values 
+(1, '123 Harbour Landing', '', 'Regina', 'A1B2C3', 'SK', 'Canada'),
+(2, '2332 Massey Rd' , 'Suite 12', 'Regina', 'H6J5K3', 'SK', 'Canada'),
+(3, '891 Robinson St', 'Apt 1', 'Regina', 'M0N4T4', 'SK', 'Canada'),
+(4, '5515 Cathedral Dr', '', 'Regina', 'J4K2N5', 'SK', 'Canada'),
+(5, '5891 Somewhere St', 'in Moose Jaw', 'Moose Jaw', 'B1N4M5', 'SK', 'Canada'),
+(6, '375 Other St', 'Suite 1', 'Ottawa','P1N3B6', 'ON', 'Canada'),
+(7, '5450 Another St', 'Apt 10', 'Brampton', 'B8S9F4', 'ON', 'Canada'),
+(8, '111 Winnipeg St', 'PO 23', 'Regina', 'N9H9C7', 'SK', 'Canada'),
+(9, '2891 Nowhere Rd', '', 'Calgary', 'T5Y7N9', 'AB', 'Canada'),
+(10, '6678 Out There Dr', '', 'Winnipeg', 'M1K4H7', 'MB', 'Canada')
+;
